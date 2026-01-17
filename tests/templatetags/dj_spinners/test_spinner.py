e32817d7c3304575f3659449f200b1d154a41266
adamghill <adamghill@yahoo.com>
2026-01-17 11:23:27 -0500

    Fix test.

--- tests/templatetags/dj_spinners/test_spinner.py
+++ tests/templatetags/dj_spinners/test_spinner.py
@@ -11,7 +11,7 @@ def test():
     assert "<svg" in result
     assert "</svg" in result
 
-    expected = '<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_qM83{animation:spinner_8HQG 1.05s infinite}.spinner_oXPr{animation-delay:.1s}.spinner_ZTLf{animation-delay:.2s}@keyframes spinner_8HQG{0%,57.14%{animation-timing-function:cubic-bezier(0.33,.66,.66,1);transform:translate(0)}28.57%{animation-timing-function:cubic-bezier(0.33,0,.66,.33);transform:translateY(-6px)}100%{transform:translate(0)}}</style><circle class="spinner_qM83" cx="4" cy="12" r="3"/><circle class="spinner_qM83 spinner_oXPr" cx="12" cy="12" r="3"/><circle class="spinner_qM83 spinner_ZTLf" cx="20" cy="12" r="3"/></svg>'  # noqa: E501
+    expected = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><style>@keyframes spinner_8HQG{0%,57.14%{animation-timing-function:cubic-bezier(.33,.66,.66,1);transform:translate(0)}28.57%{animation-timing-function:cubic-bezier(.33,0,.66,.33);transform:translateY(-6px)}to{transform:translate(0)}}.spinner_qM83{animation:spinner_8HQG 1.05s infinite}</style><circle cx="4" cy="12" r="3" class="spinner_qM83"/><circle cx="12" cy="12" r="3" class="spinner_qM83" style="animation-delay:.1s"/><circle cx="20" cy="12" r="3" class="spinner_qM83" style="animation-delay:.2s"/></svg>'  # noqa: E501
     assert expected == result
 
 
